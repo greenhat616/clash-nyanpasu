b9985b589ecc17d2ebe1a3bc36c1d15b7ed22d49
Jonson Petard <41122242+greenhat616@users.noreply.github.com>
2023-12-21 20:17:40 +0800

    chore: update template

--- scripts/prepare-nightly.ts
+++ scripts/prepare-nightly.ts
@@ -17,6 +17,9 @@ const PACKAGE_JSON_PATH = path.join(cwd, "package.json");
 // blocked by https://github.com/tauri-apps/tauri/issues/8447
 // const WXS_PATH = path.join(TAURI_APP_DIR, "templates", "nightly.wxs");
 
+const isNSIS = process.argv.includes("--nsis"); // only build nsis
+const isMSI = process.argv.includes("--msi"); // only build msi
+
 async function main() {
   consola.debug("Read config...");
   const tauriAppConf = await fs.readJSON(TAURI_APP_CONF);
@@ -24,6 +27,12 @@ async function main() {
   const tauriConf = merge(tauriAppConf, tauriAppOverrides);
   const packageJson = await fs.readJSON(PACKAGE_JSON_PATH);
   // const wxsFile = await fs.readFile(WXS_PATH, "utf-8");
+
+  if (isNSIS) {
+    tauriConf.tauri.bundle.targets = ["nsis"];
+  } else if (isMSI) {
+    tauriConf.tauri.bundle.targets = ["msi"];
+  }
 
   consola.debug("Get current git short hash");
   const GIT_SHORT_HASH = execSync("git rev-parse --short HEAD")
@@ -43,7 +52,7 @@ async function main() {
   // consola.debug("wxs updated");
   // 2. update tauri version
   consola.debug("Write tauri version to tauri.nightly.conf.json");
-  tauriConf.package.version = version;
+  if (!isMSI) tauriConf.package.version = version;
   await fs.writeJSON(TAURI_DEV_APP_CONF_PATH, tauriConf, { spaces: 2 });
   consola.debug("tauri.nightly.conf.json updated");
   // 3. update package version
